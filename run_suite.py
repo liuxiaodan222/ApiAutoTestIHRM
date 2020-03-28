fad40b3dad8b03873c06060482ed36e9a5307c7e
Your Name <email@example.com>
2020-03-28 15:26:24 +0800

    修改报告名称

--- run_suite.py
+++ run_suite.py
@@ -11,7 +11,8 @@ suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestLogin))
 suite.addTest(unittest.makeSuite(TestEmplyoee))
 
-report_name = app.BASE_DIR + "/report/ihrm_{}.html".format(time.strftime("%Y%m%d_%H%M%S"))
+# report_name = app.BASE_DIR + "/report/ihrm_{}.html".format(time.strftime("%Y%m%d_%H%M%S"))
+report_name = app.BASE_DIR + "/report/ihrm.html"
 with open(report_name, "wb") as f:
     runner = HTMLTestRunner(stream=f, verbosity=2, title="IHRM人力资源管理系统", description="HTMLTestRunner_PY3版本报告")
     runner.run(suite)
